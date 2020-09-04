95ad40d5a97071a24a3e4c2ccfd609cf449c9e97
luckky <guodashun@163.com>
2020-09-04 10:40:31 +0800

    learnt nSum

---
+++ basic/nSum.cpp
@@ -0,0 +1,30 @@
+vector<vector<int>> nSumTarget(vector<int>& nums, int n, int start, int target) {
+    int sz = nums.size();
+    vector<vector<int>> res;
+    if (n < 2 || sz < n) return res;
+    if (n == 2) {
+        int l = start, r = sz - 1;
+        while(l < r) {
+            int sum = nums[l] + nums[r];
+            if (sum < target) {
+                while(l < r && nums[l] == nums[l+1]) l++;
+            } else if (sum > target) {
+                while(l < r && nums[r] == nums[r-1]) r--;
+            } else {
+                res.push_back({l, r});
+                while(l < r && nums[l] == nums[l+1]) l++;
+                while(l < r && nums[r] == nums[r-1]) r--;
+            }
+        }
+    } else {
+        for (int i = start; i < sz; i++) {
+            vector<vector<int>> sub = nSumTarget(nums, n-1, i+1, target - nums[i]); // 重点
+            for (vector<int>& arr : sub) {
+                arr.push_back(nums[i]);
+                res.push_back(arr);
+            }
+            while(i < sz - 1 && nums[i] == nums[i+1]) i++; // 这一步不能省
+        }
+    }
+    return res;
+}
