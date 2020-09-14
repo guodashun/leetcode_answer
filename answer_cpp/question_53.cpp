42dd5e13f5ffceac250519f164926318a1f106e4
luckky <guodashun@163.com>
2020-09-14 09:11:36 +0800

    add 53 with dp

--- answer_cpp/question_53.cpp
+++ answer_cpp/question_53.cpp
@@ -45,4 +45,26 @@ public:
 2. 使用auto num后 过多的对num的索引会增加耗时； 
 */
 
+// 动归解法
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        // base case
+        int dp_0 = nums[0];
+        int dp_1 = 0, res = dp_0;
 
+        for (int i = 1; i < n; i++) {
+            // dp[i] = max(nums[i], nums[i] + dp[i-1])
+            dp_1 = max(nums[i], nums[i] + dp_0);
+            dp_0 = dp_1;
+            // 顺便计算最大的结果
+            res = max(res, dp_1);
+        }
+
+        return res;
+    }
+};
+
+// 时间啥的无所谓，只要复杂度够小就行，太追求耗时和内存只会让代码看起来更难懂
