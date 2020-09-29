98d18f1da3e7f11a9aa232d0e012a359a2cf4e31
luckky <guodashun@163.com>
2020-09-29 09:23:19 +0800

    learnt dp

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Answers and notes 4 leetcode :)
 
 ### Dynamic Programming(DP)
 
+备忘录：509/322
+
+字符串：1312
---
+++ answer_cpp/question_1312.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+
+        int n = s.size();
+        vector<int> dp(n, 0);
+
+        int temp = 0;
+        for (int i = n - 2; i >= 0; i--) {
+            // 记录 dp[i+1][j-1]
+            int pre = 0;
+            for (int j = i + 1; j < n; j++) {
+                temp = dp[j];
+
+                if (s[i] == s[j]) {
+                    // dp[i][j] = dp[i+1][j-1];
+                    dp[j] = pre;
+                } else {
+                    // dp[i][j] = min(dp[i+1][j], dp[i][j-1]) + 1;
+                    dp[j] = min(dp[j], dp[j - 1]) + 1;
+                }
+
+                pre = temp;
+            }
+        }
+
+        return dp[n - 1];
+
+    }
+};
+
+// 字符串一般用二维数组
