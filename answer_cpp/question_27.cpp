bb5ab95232e488ec4b7870e90d30fe60c5e14bc9
luckky <guodashun@163.com>
2020-10-11 09:53:58 +0800

    add 27

---
+++ answer_cpp/question_27.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int fast = 0, slow = 0;
+        while (fast < nums.size()) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+};
