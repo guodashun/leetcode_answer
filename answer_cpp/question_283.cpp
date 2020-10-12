ff8cb1e53e33bdc691da4078751712299ad52f10
luckky <guodashun@163.com>
2020-10-12 10:29:26 +0800

    add 283

---
+++ answer_cpp/question_283.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 1) {
+            return;
+        }
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        for (int i = index; i < n; i++) {
+            nums[i] = 0;
+        }
+    }
+};
+
+//
