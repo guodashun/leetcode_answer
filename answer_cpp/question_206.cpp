0070fcaf57db84d4abe11f7d899d34ece1819617
luckky <guodashun@163.com>
2020-11-02 14:06:19 +0800

    reverse list

---
+++ answer_cpp/question_206.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL || head->next == NULL) {
+            return head;
+        }
+        ListNode* ret = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return ret;
+    }
+};
+
+// 一直return ret是为了保存最后一个节点，最后返回reverse后的head
+// 重要的地方在于对head以及对head next的处理
