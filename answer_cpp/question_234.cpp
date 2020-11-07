d6213b7ea20b6a7b30029e52c97fb726e84c32be
luckky <guodashun@163.com>
2020-11-07 11:09:48 +0800

    judge list

---
+++ answer_cpp/question_234.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> vals;
+        while (head != nullptr) {
+            vals.emplace_back(head->val);
+            head = head->next;
+        }
+        for (int i = 0, j = (int)vals.size() - 1; i < j; ++i, --j) {
+            if (vals[i] != vals[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
