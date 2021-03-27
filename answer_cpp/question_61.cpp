ec84684d0a1286e6209412a3d0940fbc70a765e1
luckky <guodashun@163.com>
2021-03-27 10:22:31 +0800

    list structure

---
+++ answer_cpp/question_61.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (k == 0 || head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        int n = 1;
+        ListNode* iter = head;
+        while (iter->next != nullptr) {
+            iter = iter->next;
+            n++;
+        }
+        int add = n - k % n;
+        if (add == n) {
+            return head;
+        }
+        iter->next = head;
+        while (add--) {
+            iter = iter->next;
+        }
+        ListNode* ret = iter->next;
+        iter->next = nullptr;
+        return ret;
+    }
+};
+
+// base 只考虑了一种情况，其实有三种（head 和 head->next 算是一种情况）
+// 记录链表长度n（不用stack，因为本身链表可以拿到每个值）
+// ListNode* 记录位置，不会随着新变量的引入而随着新变量发生变化
+// 只有修改链表的next，才会改变链表的结构
