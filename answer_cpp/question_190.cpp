d34e66753eeb7a918c2a9b8e96e443bb05cb47ac
luckky <guodashun@163.com>
2021-03-29 10:19:56 +0800

    hands up

---
+++ answer_cpp/question_190.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t rev = 0;
+        for (int i = 0; i < 32 && n > 0; ++i) {
+            rev |= (n & 1) << (31 - i);
+            n >>= 1;
+        }
+        return rev;
+    }
+};
+
+// (n & 1) 取最后一位
+// n >>= 1 -> n /= 2
